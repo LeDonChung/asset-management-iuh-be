3132adba48f42bcd041ef1a358bbeae7b0e6962d
thahhtuyenn <thanhtuyen9623@gmail.com>
2025-09-24 21:23:57 +0700

    feature/user: write api change status (locked or unlocked) user

--- src/modules/users/dto/update-user.dto.ts
+++ src/modules/users/dto/update-user.dto.ts
@@ -1,6 +1,19 @@
-import { PartialType } from "@nestjs/swagger";
+import { ApiProperty, PartialType } from "@nestjs/swagger";
 import { CreateUserDto } from "./create-user.dto";
+import { UserStatus } from "src/entities/user.entity";
+import { IsEnum, IsNotEmpty } from "class-validator";
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
-    
+
+}
+
+export class UpdateUserStatusDto {
+    @ApiProperty({
+        example: UserStatus.ACTIVE,
+        description: 'User status',
+        enum: UserStatus
+    })
+    @IsEnum(UserStatus, { message: 'Status must be a valid UserStatus value' })
+    @IsNotEmpty()
+    status: UserStatus;
 }
