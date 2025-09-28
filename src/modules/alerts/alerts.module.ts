1a983c69fbd04ebb48ebc22454200c17c8d3f8c2
Le Chung <ledonchung12a2@gmail.com>
2025-09-28 22:56:33 +0700

    feat: add user RFID alerts endpoint and response DTO
    - Implemented a new endpoint to retrieve user RFID alerts in AlertsController.
    - Added UserAlertResponseDto for structured response.
    - Updated AlertsService to handle user RFID alerts logic.
    - Enhanced AlertsModule to include necessary entities for the new functionality.

--- src/modules/alerts/alerts.module.ts
+++ src/modules/alerts/alerts.module.ts
@@ -7,10 +7,13 @@ import { User } from 'src/entities/user.entity';
 import { Room } from 'src/entities/room.entity';
 import { AlertsService } from './alerts.service';
 import { AlertsController } from './alerts.controller';
+import { AssetBook } from 'src/entities/asset-book.entity';
+import { RfidTag } from 'src/entities/rfid-tag.entity';
+import { AssetBookItem } from 'src/entities/asset-book-item.entity';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Alert, AlertResolution, Asset, User, Room])
+        TypeOrmModule.forFeature([Alert, AlertResolution, Asset, User, Room, AssetBook, AssetBookItem, RfidTag])
     ],
     controllers: [AlertsController],
     providers: [AlertsService],
